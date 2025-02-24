955b67b7b4a2fddc2d8a64ac84e3d19840d1abfa
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-02-24 13:03:19 +0100

    Implement Homepage

---
+++ Untitled-1.md
@@ -0,0 +1,11 @@
+Initial setup:
+  ```shell
+  mkdir module3
+  cd module3
+  mkdir iron-restaurant
+  cd iron-restaurant
+  touch app.js
+  npm init --yes
+  code -r .
+  npm install express
+  ```
---
+++ app.js
@@ -0,0 +1,69 @@
+// commonJS
+
+
+const express = require("express");
+const pizzasArr = require("./data/pizzas.js");
+
+
+const app = express();
+
+// route- app.get(path,code)
+//GET http://localhost:3000/
+
+// Make the static files inside of the `public/` folder publicly accessible
+app.use(express.static('public'));
+
+// don't include public in path. just start with images
+
+//Example of a middleware function...
+
+function customMiddleware1(req,res,next){
+    console.log("hello 1..");
+    if(consition){
+        next(); //invoke the next middleware function
+    } else {
+        response.send("Sorry, no condition")
+    }
+}
+
+function customMiddleware2(req,res,next){
+    console.log("hello 2..");
+    next();
+}
+
+app.use(customMiddleware1)
+app.use(customMiddleware2)
+
+
+
+app.get("/", (request, response, next) => {
+  /*  console.log("We've received a GET request to /");
+response.send(`homepage...`)*/
+response.sendFile(__dirname +"/views/home.html");
+})
+
+app.get("/contact", (request, response, next) => {
+    console.log("We've received a GET request to /contact");
+   /* console.log(request.method);
+    console.log(request.path);
+    console.log(request.protocol);
+    response.send(" Good morning")
+    response.send(``)*/
+    /* you cannot send 2 response 
+    like: response. send("Hello")
+    response.send("Hi")*/
+    response.sendFile(__dirname +"/views/contact.html");
+})
+
+
+
+app.get("/pizza", (request,response,next) => {
+
+response.json(pizzasArr);
+})
+
+
+
+app.listen(3000, ()=> {
+    console.log("Server listening on port .... 3000");
+});
